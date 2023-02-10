382278f2f89fa0b826bb2905a70f7bef481ce146
Jun Hao <junhaoliucaie@gmail.com>
2023-02-10 23:59:29 +0100

    add entrance loading

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './css/app.css'
-import { Header, Examples, MoreFeatures, WhatAudioGPT3, Possibility, Footer} from './container'
-import { CTA, Brand, Navbar} from './components';
+import { Header, Examples, MoreFeatures, WhatAudioGPT3, Possibility, Footer } from './container'
+import { CTA, Brand, Navbar } from './components';
+import PuffLoader from "react-spinners/PuffLoader";
 
 
-const app = () => {
+const App = () => {
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3500);
+  }, [])
+
   return (
     <div className='MainApp'>
-      <div className='bg'>
-      <Navbar />
-      <Header />
-      </div>
-      <Brand />
-      <WhatAudioGPT3 />
-      <MoreFeatures />
-      <Possibility />
-      <CTA />
-      <Examples />
-      <Footer />
+      {
+        loading ?
+          (
+          <div className='load'>
+            <PuffLoader
+            color={"#AE67FA"}
+            loading={loading}
+            size={250}
+          />
+          <h1>Loading....</h1>
+          </div>)
+          : (<div>
+            <div className='bg'>
+              <Navbar />
+              <Header />
+            </div>
+            <Brand />
+            <WhatAudioGPT3 />
+            <MoreFeatures />
+            <Possibility />
+            <CTA />
+            <Examples />
+            <Footer />
+          </div>
+          )
+      }
+
     </div>
-    
+
   )
 }
 
-export default app
+export default App
